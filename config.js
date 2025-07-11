3891ecfb95bd53cb69e295fa5980d30044f456e3
Dewdushyanth <pgtharushdew@gmail.com>
2025-07-11 13:55:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://res.cloudinary.com/dtjjgiitl/image/upload/q_auto:good,f_auto,fl_progressive/v1752221911/ygjvefkwa055biwyirnk.jpg", 
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW THIS IS DEW CYBER AREA..! im alive now💀",
 };
