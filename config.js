bd618e489b4eef55f5aa4bc85eee66aa207c5039
Dewdushyanth <pgtharushdew@gmail.com>
2025-07-12 12:08:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,38 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://res.cloudinary.com/dtjjgiitl/image/upload/q_auto:good,f_auto,fl_progressive/v1752221911/ygjvefkwa055biwyirnk.jpg", 
-ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW THIS IS DEW CYBER AREA..! im alive now💀",
-};
+    SESSION_ID: process.env.SESSION_ID || "pLlV1TDR#tTOtE60OG0IGsQXo6fKOKfVr2K6Z2212y_oQNSEaUIY",
+    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
+    CAPTION: process.env.CAPTION || "*ᴀᴋɪɴᴅᴜ-ᴍᴅ*",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
+    AUTO_TYPING: process.env.AUTO_TYPING || "false",
+    CURRENT_STATUS: process.env.CURRENT_STATUS || "false",
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "false",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTHOR: (process.env.PACK_INFO?.split(';') || [])[0] || 'dew md',
+    PACKNAME: (process.env.PACK_INFO?.split(';') || [])[1] || '94 78 781 81 71',
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/wqp3y9.jpg",
+    ANTI_LINK: process.env.ANTI_LINK || "false",
+    ANTI_BAD: process.env.ANTI_BAD || "false",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
+    BOT_NAME: process.env.BOT_NAME || "*DEW CYBER AREA*",
+    ANTI_VV: process.env.ANTI_VV || "true",
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", // change it to 'same' if you want to resend deleted message in same chat
+    OWNER_NAME: process.env.OWNER_NAME || "ᴀᴋɪɴᴅᴜ", //replace with your name 
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "94 78 781 8171",// add your bot owner number
+    DEV: process.env.DEV || "94764703165", //replace with your whatsapp number
+    AUTOLIKESTATUS: process.env.AUTOLIKESTATUS || "true",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`ꜱᴛᴀᴛᴜꜱ ꜱᴇᴇɴ ʙʏ ᴀᴋɪɴᴅᴜ-ᴍᴅ ʙᴏᴛ`",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+    };
